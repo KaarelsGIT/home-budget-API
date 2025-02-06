4f0ae617e16bc19ebcb9cf2ae71c22eb548d0450
kaarelkattai <kaarel.kattai@hotmail.com>
2025-02-06 11:14:51 +0200

    added components and layout with header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {IncomesComponent} from './pages/incomes/incomes.component';
+import {ExpensesComponent} from './pages/expenses/expenses.component';
+import {SettingsComponent} from './pages/settings/settings.component';
+import {AboutComponent} from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'summary', component: DashboardComponent },
+  { path: 'incomes', component: IncomesComponent },
+  { path: 'expenses', component: ExpensesComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'about', component: AboutComponent }
+];
