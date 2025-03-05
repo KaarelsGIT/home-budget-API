51db817c4618f8962bea1689f1cff53547da79ef
kaarelkattai <kaarel.kattai@hotmail.com>
2025-03-06 00:37:24 +0200

    minor update and start creating update transaction form!

---
+++ src/app/components/shared/transaction-update-form/transaction-update-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-update-form',
+  imports: [],
+  templateUrl: './transaction-update-form.component.html',
+  styleUrl: './transaction-update-form.component.css'
+})
+export class TransactionUpdateFormComponent {
+
+}
