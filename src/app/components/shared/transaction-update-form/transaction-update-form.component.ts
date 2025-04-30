500cee001caa3a2cc7d29dc1dcbe3104c6ee922d
kaarelkattai <kaarel.kattai@gmail.com>
2025-04-30 08:03:26 +0300

    one way to update transaction

--- src/app/components/shared/transaction-update-form/transaction-update-form.component.ts
+++ src/app/components/shared/transaction-update-form/transaction-update-form.component.ts
@@ -1,18 +1,24 @@
-import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
+import {CategoryService} from '../../../services/category.service';
+import {Category} from '../../../models/category';
+import {User} from '../../../models/user';
+import {UserService} from '../../../services/user.service';
 
 @Component({
   selector: 'app-transaction-update-form',
   standalone: true,
   imports: [
     FormsModule,
-    NgIf
+    NgIf,
+    NgForOf
   ],
   templateUrl: './transaction-update-form.component.html',
   styleUrl: './transaction-update-form.component.css'
 })
-export class TransactionUpdateFormComponent implements OnChanges {
+export class TransactionUpdateFormComponent implements OnChanges, OnInit {
+  @Input() type!: 'income' | 'expense';
   @Input() transaction: any;
   @Input() isVisible = false;
   @Output() close = new EventEmitter<void>();
@@ -20,10 +26,34 @@ export class TransactionUpdateFormComponent implements OnChanges {
 
   isLoading = false;
 
+  categories: Category[] = [];
+  users: User[] = [];
+
   ngOnChanges(changes: SimpleChanges) {
     if (changes['isVisible'] && changes['isVisible'].currentValue === true) {
       this.isLoading = false;
     }
+  }
+
+  ngOnInit(){
+    this.loadCategories();
+    this.loadUsers();
+  }
+
+  constructor(private categoryService: CategoryService,
+              private userService: UserService,) {
+  }
+
+  loadCategories() {
+    this.categoryService.getCategoriesByType(this.type, true).subscribe(categories => {
+      this.categories = categories;
+    });
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
   }
 
   onSave() {
@@ -37,5 +67,4 @@ export class TransactionUpdateFormComponent implements OnChanges {
     this.isLoading = false;
     this.close.emit();
   }
-
 }
